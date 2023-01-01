86f1b29e724e817f7708ac91a05ca66709a474ec
9William <gwilliam@student.telkomuniversity.ac.id>
2023-01-02 04:59:32 +0700

    Working Login Database

--- dmc_back_end/index.js
+++ dmc_back_end/index.js
@@ -8,6 +8,11 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use("/js", express.static("../assets/js"))
+app.use("/css", express.static("../assets/css"))
+app.use("/js", express.static("../js"))
+app.use("/css", express.static("../css"))
+
 app.use(cors({origin: 'null'}))
 app.use(express.json())
 app.use(router)
--- dmc_back_end/router.js
+++ dmc_back_end/router.js
@@ -4,9 +4,11 @@ const connection = require('./database')
 const router = new express.Router()
 const message = "drugs is gucci"
 
+const pars = require("body-parser")
+const encoder = pars.urlencoded({ extended: true })
+
 router.get('/', (req, res) => {
-    // buatlah perintah untuk mengirimkan response berupa Nama - NIM
-    res.send(message)
+    res.sendFile("index.html", {root: "../"});
 })
 
 router.get('/getAllPlanes', (req, res) => {
@@ -18,59 +20,91 @@ router.get('/getAllPlanes', (req, res) => {
             res.status = 500;
         } else {
             console.log("miomio");
-            res.status(200).send({data:rows});
         }
     });
 })
 
-router.get('/getMahasiswa/:id', (req, res) => {
-    const id = req.params.id
-    // buatlah perintah untuk mengambil data berdasarkan id pada table mahasiswa, jika berhasil mengirimkan response 200 dan gagal status 500
-    connection.query('SELECT * FROM mahasiswa', (error, rows) => {
-    
-        if (error) {
-            console.log(error);
-            response.status = 500;
+router.post('/',  encoder, (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    connection.query('SELECT email, password FROM user WHERE email = ? AND password = ?', [email, password], (error, result, fields) => {
+        if (result.length > 0) {
+            res.redirect('/homeActivity')
         } else {
-            console.log("miomio");
-            res.status(200).send({data:rows});
+            res.redirect('/')
         }
-    });
-})
-
-router.post('/insertMahasiswa', (req, res) => {
-    const nama = req.body.nama
-    const nim = req.body.nim
-    const jurusan = req.body.jurusan
-
-    connection.query( 'INSERT INTO mahasiswa (id, nim, nama, jurusan) values(id,?,?,?)' , 
-        [nim, nama, jurusan], 
-        (error, rows, fields) => {
-            if (error) {
-                console.log(error);
-                response.status = 500;
-            } else {
-                console.log("miomio");
-                res.status(200).send({data:rows});
-            }
+        res.end();
     })
 })
 
-router.post('/updateMahasiswa/:id', (req, res) => {
-    const id = req.params.id
-    const nama = req.body.nama
-    const nim = req.body.nim
-    const jurusan = req.body.jurusan
-    
-    // buatlah perintah untuk mengupdate nama, email, dan jurusan berdasarkan id pada table mahasiswa jika berhasil status 200 dan gagal 400
-    
+router.get('/homeActivity', (req, res) => {
+    res.sendFile("homeActivity.html", {root: "../"})
 })
 
-router.post('/deleteMahasiswa/:id', (req, res) => {
-    const id = req.params.id
-    
-    // buatlah perintah untuk menghapus data berdasarkan id pada table mahasiswa jika berhasil status 200 dan gagal status 500
-    
+router.get('/index.html', (req, res) => {
+    res.redirect('/')
 })
+
+router.get('/register', (req, res) => {
+    res.sendFile('indexReg.html', {root: '../'})
+})
+
+router.post('/register',  encoder, (req, res) => {
+    const nama = req.body.nama
+    const email = req.body.email;
+    const password = req.body.password;
+    connection.query('INSERT INTO user (nama, email, password) values (?, ?, ?)', [nama, email, password], (error, result, fields) => {
+        if (error) {
+            console.log(error)
+        } else {
+            res.redirect('/homeActivity')
+        }
+        res.end();
+    })
+})
+
+// router.post('/insertMahasiswa', (req, res) => {
+//     const nama = req.body.nama
+//     const nim = req.body.nim
+//     const jurusan = req.body.jurusan
+
+//     // buatlah perintah untuk menambahkan nama, nim, dan jurusan pada table mahasiswa jika berhasil status 200 dan gagal status 400
+//     connection.query('INSERT INTO mahasiswa values (?,?,?,?)', [0, nama, nim, jurusan] , (error, rows) => {
+//         if (error) {
+//             console.log(error)
+//         } else {
+//             res.status(200).json({message:"", data:rows})
+//         }
+//     })
+// })
+
+// router.post('/updateMahasiswa/:id', (req, res) => {
+//     const id = req.params.id
+//     const nama = req.body.nama
+//     const nim = req.body.nim
+//     const jurusan = req.body.jurusan
+    
+//     // buatlah perintah untuk mengupdate nama, email, dan jurusan berdasarkan id pada table mahasiswa jika berhasil status 200 dan gagal 400
+//     connection.query('UPDATE mahasiswa SET nim = ?, nama = ?, jurusan = ? WHERE id = ?', [nim, nama, jurusan, id] , (error, rows) => {
+//         if (error) {
+//             console.log(error)
+//         } else {
+//             res.status(200).json({message:"", data:rows})
+//         }
+//     })
+// })
+
+// router.post('/deleteMahasiswa/:id', (req, res) => {
+//     const id = req.params.id
+    
+//     // buatlah perintah untuk menghapus data berdasarkan id pada table mahasiswa jika berhasil status 200 dan gagal status 500
+//     connection.query('DELETE FROM mahasiswa WHERE id = ?', id , (error, rows) => {
+//         if (error) {
+//             console.log(error)
+//         } else {
+//             res.status(200).json({message:"", data:rows})
+//         }
+//     })
+// })
 
 module.exports = router
